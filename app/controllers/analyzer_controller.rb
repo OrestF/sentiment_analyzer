9f20dc23af137dfcabe19161c710b96837c7db16
OrestF <falchuko@gmail.com>
2019-06-23 16:44:38 +0300

    Add default message

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -2,12 +2,20 @@
 
 class AnalyzerController < ApplicationController
   def create
-    render json: Analyzer.call(text)
+    render json: text.blank? ? default_message : analyzed_response
   end
 
   private
 
   def text
     params[:text]
+  end
+
+  def default_message
+    { message: "This is sentiment analyzer. GET or POST this endpoint with 'text' param and get sentiment analysis" }
+  end
+
+  def analyzed_response
+    Analyzer.call(text)
   end
 end
